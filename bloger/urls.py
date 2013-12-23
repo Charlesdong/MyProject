cd1bca066542faad08f9d212084cf9388d9e4d84
dongfanliang <dongfanliang@DONGFANLIANG>
2013-12-23 17:17:20 +0800

    add

--- bloger/urls.py
+++ bloger/urls.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from django.conf.urls import patterns, url
 
-from bloger.views import BlogDetailView, BlogCommentView, CaptchaAjax
+from bloger.views import BlogDetailView, BlogCommentView, test
 
 
 urlpatterns = patterns('',
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
    # url(r'^article/list/$', BlogListView.as_view(), name="article-list"),
     url(r'^article/detail/(?P<pk>\d+)/$', BlogDetailView.as_view(), name="article-detail"),
     url(r'^article/comment/$', BlogCommentView.as_view(), name="blog-comment"),
-    url(r'^captcha/ajax/$', CaptchaAjax.as_view(), name="captcha-ajax"),
+    url(r'^comment/test/$', test, name="test"),
 )
