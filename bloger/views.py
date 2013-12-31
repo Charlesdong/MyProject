a7d874900ff803e05b6f70b69a5e11f3003ac9f7
dongfanliang <dongfanliang@DONGFANLIANG>
2013-12-31 17:27:17 +0800

    aa

--- bloger/views.py
+++ bloger/views.py
@@ -1,11 +1,5 @@
 # coding=utf-8
-from captcha.helpers import captcha_image_url
-from captcha.models import CaptchaStore
-from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.views.generic import TemplateView, DetailView, ListView, CreateView
-from bloger.renders import JSONResponseMixin
+from django.views.generic import TemplateView, DetailView, ListView
 from data.models import BlogArticle
 
 
@@ -33,19 +27,6 @@ class BlogListView(ListView):
     def get_queryset(self):
         classfy_id = self.request.GET.get('classfy_id', None)
 
-        queryset = self.model.objects.all()
-        if classfy_id:
-            queryset = queryset.filter(classify_id=classfy_id)
-        return queryset
-
-
-class BlogLifeListView(ListView):
-
-    model = BlogArticle
-    paginate_by = 10
-    context_object_name = 'object_list'
-
-    def get_queryset(self):
         queryset = self.model.objects.all()
         if classfy_id:
             queryset = queryset.filter(classify_id=classfy_id)
